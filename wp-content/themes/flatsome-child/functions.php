6d9ff75eb8c9f1002c02f5f45399f068673fbb97
AttractMore <roger@rogerknight.info>
2022-06-17 16:23:20 +0200

    Update redsys plugin. Hide product category from site

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,19 @@
 <?php
-// Add custom Theme Functions here
+/**************************************************************
+  Stop certain product categories from being displayed. See https://wpdatatables.com/woocommerce-hide-category-from-your-shop-page/
+**************************************************************/
+add_filter( 'get_terms', 'am_get_subcategory_terms', 10, 3 );
+
+function am_get_subcategory_terms( $terms, $taxonomies, $args ) {
+  $new_terms = array();
+  // if it is a product category and on the shop page
+  if ( in_array( 'product_cat', $taxonomies ) && ! is_admin() && is_shop() ) {
+    foreach( $terms as $key => $term ) {
+      if ( !in_array( $term->slug, array( 'jarapas-alpujarrenas' ) ) ) { //pass the slug name here
+        $new_terms[] = $term;
+      }
+    }
+    $terms = $new_terms;
+  }
+  return $terms;
+}
