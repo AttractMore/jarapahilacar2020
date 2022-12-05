e4da89af17399bcaa91fa43ab5c8fa25ceaf695b
Roger Knight <roger@rogerknight.info>
2022-12-05 15:51:59 +0100

    Hide category pelo extra

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,7 +1,8 @@
 <?php
-/**************************************************************
-  Stop certain product categories from being displayed. See https://wpdatatables.com/woocommerce-hide-category-from-your-shop-page/
-**************************************************************/
+/**
+*  @snippet Stop certain product categories from being displayed. 
+*  @source  https://wpdatatables.com/woocommerce-hide-category-from-your-shop-page/
+*/
 add_filter( 'get_terms', 'am_get_subcategory_terms', 10, 3 );
 
 function am_get_subcategory_terms( $terms, $taxonomies, $args ) {
@@ -9,7 +10,7 @@ function am_get_subcategory_terms( $terms, $taxonomies, $args ) {
   // if it is a product category and on the shop page
   if ( in_array( 'product_cat', $taxonomies ) && ! is_admin() && is_shop() ) {
     foreach( $terms as $key => $term ) {
-      if ( !in_array( $term->slug, array( 'jarapa-granada', 'festival-de-ecoturismo' ) ) ) { //pass the slug names here
+      if ( !in_array( $term->slug, array( 'jarapa-granada', 'festival-de-ecoturismo', 'jarapas-pelo-extra' ) ) ) { //pass the slug names here
         $new_terms[] = $term;
       }
     }
